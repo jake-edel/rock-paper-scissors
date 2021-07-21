bf7a989802622c762093f29c8bbad65d08762980
Jakob Edelstein <jakobedelstein@gmail.com>
2021-07-20 18:30:43 -0600

    Create game() function, force user input to lowercase

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,10 +1,11 @@
 
+
+//Return a random integer between 1 and 3
 function getRandomIntInclusive(min, max) {
-//    min = Math.ceil(min);
-//    max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
   }
 
+//Take random integer and apply either rock, paper or scissors.
 function computerPlay() {
     var hand = getRandomIntInclusive(0,2)
     //console.log(hand);
@@ -19,7 +20,7 @@ function computerPlay() {
     return hand;
 }
 
-
+//Take computers and players hand and compare for a winner
 function playRound (playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         console.log("Tie Game!")
@@ -27,24 +28,37 @@ function playRound (playerSelection, computerSelection) {
     || (playerSelection === "paper" && computerSelection === "scissors") 
     || (playerSelection === "scissors" && computerSelection === "rock")) {
         console.log(("You lose!"))
+        computerWins++;
     } else if ((playerSelection === "rock" && computerSelection === "scissors") 
     || (playerSelection === "paper" && computerSelection === "rock") 
     || (playerSelection === "scissors" && computerSelection === "paper")) {
         console.log("You win!")
+        playerWins++;
     }
 }
 
+//Play through game 5 times
 function game() {
 
-
     for(i = 0; i < 5; i++) {
-        var playerInput = window.prompt("Select rock, paper or scissors").toLowerCase;
-        //playerHand = playerHand.toLowerCase;
-        var playerHand = playerInput.toLowerCase;
-        console.log(`Player hand: ${playerHand}`)
+        var playerInput = window.prompt("Select rock, paper or scissors").toLowerCase();
+        console.log(`Player hand: ${playerInput}`)
+        playRound(playerInput, computerPlay());
+        //playRound("rock", "scissors");
 
-        playRound(playerHand, computerPlay());
+        console.log(`Computer: ${computerWins} || Player: ${playerWins}`);
     };
 }
 
+var computerWins = 0;
+var playerWins = 0;
+
 game();
+
+if (computerWins > playerWins) {
+    console.log("Computer wins!");
+} else if (computerWins < playerWins) {
+    console.log("Player Wins");
+} else {
+    console.log("Tie Game!")
+}

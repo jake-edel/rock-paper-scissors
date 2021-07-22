5c48c8974087667ffc1b4daa937fa5b58f3d90e8
Jakob Edelstein <jakobedelstein@gmail.com>
2021-07-21 18:02:39 -0600

    Add UI elements for computer choice/winner, add icons to buttons, install custom fonts

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM Content Loaded")
 
+    const winner = document.querySelector('#game-results')
+    const computerHand = document.querySelector('#computer-hand')
+
     const buttons = document.querySelectorAll('button');
     console.log(buttons)
 
@@ -10,55 +13,58 @@ document.addEventListener('DOMContentLoaded', () => {
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
             var playerInput = button.id;
-            game(playerInput, computerPlay());
+            playRound(playerInput, computerPlay());
             console.log(`Player Score: ${playerScore} || Computer Score: ${computerScore}`)
         });
     });
 
 
 
-function game(player, computer) {
-    playRound(player, computer);
-    //console.log(`Player: ${player} || Computer: ${computer}`);
-}
 
-//Take computers and players hand and compare for a winner
-function playRound (player, computer) {
-    if (player === computer) {
-        //console.log("Tie Game!")
-    } else if ((player === "sword" && computer == "shield") 
-    || (player === "shield" && computer === "magic") 
-    || (player === "magic" && computer === "sword")) {
-        //console.log(("You lose!"))
-        computerScore++;
-    } else if ((player === "sword" && computer === "magic") 
-    || (player === "shield" && computer === "sword") 
-    || (player === "magic" && computer === "shield")) {
-        //console.log("You win!")
-        playerScore++;
+
+    //Take computers and players hand and compare for a winner
+    function playRound (player, computer) {
+        if (player === computer) {
+            winner.textContent = "An even match"
+            console.log("Tie Game!")
+        } else if ((player === "sword" && computer == "shield") 
+        || (player === "shield" && computer === "magic") 
+        || (player === "magic" && computer === "sword")) {
+            winner.textContent = "Computer Wins"
+            console.log(("You lose!"))
+            computerScore++;
+        } else if ((player === "sword" && computer === "magic") 
+        || (player === "shield" && computer === "sword") 
+        || (player === "magic" && computer === "shield")) {
+            winner.textContent ="Player Wins"
+            console.log("You win!")
+            playerScore++;
+        }
     }
-}
 
+    //Return a random integer between 1 and 3
+    function getRandomIntInclusive(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    //Take random integer and apply either rock, paper or scissors.
+    function computerPlay() {
+
+
+        var hand = getRandomIntInclusive(0,2)
+        if (hand === 0) {
+            hand = "sword"
+        } else if (hand === 1) {
+            hand = "shield"
+        } else if (hand = 2) {
+            hand = "magic"
+        };
+        computerHand.textContent = `Computer chooses ${hand}`
+        return hand;
+    }
 
 });
 
 
 
                                                                     //Logic for Computer
-//Return a random integer between 1 and 3
-function getRandomIntInclusive(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-//Take random integer and apply either rock, paper or scissors.
-function computerPlay() {
-    var hand = getRandomIntInclusive(0,2)
-    if (hand === 0) {
-        hand = "sword"
-    } else if (hand === 1) {
-        hand = "shield"
-    } else if (hand = 2) {
-        hand = "magic"
-    };
-    return hand;
-}
